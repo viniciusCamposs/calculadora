4454a2d414c096cb8a35422bed90c0afe533ff1e
Vinicius <54860458+viniciusCamposs@users.noreply.github.com>
2020-08-29 01:27:42 -0300

    calculadora-02

--- calculadora-02/src/calculadora.java
+++ calculadora-02/src/calculadora.java
@@ -11,7 +11,7 @@ import javax.swing.border.EtchedBorder;
  *
  * @author Vinícius Alves de Campos
  */
-public class NewJFrame extends javax.swing.JFrame {
+public class calculadora extends javax.swing.JFrame {
 
     double num1, num2, resultado;
     char operacao;
@@ -49,7 +49,7 @@ public class NewJFrame extends javax.swing.JFrame {
         }
     }
 
-    public NewJFrame() {
+    public calculadora() {
         initComponents();
         this.setLocationRelativeTo(null);
         lblTela.setBorder(new EtchedBorder());
@@ -436,20 +436,21 @@ public class NewJFrame extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(calculadora.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(calculadora.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(calculadora.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(calculadora.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new NewJFrame().setVisible(true);
+                new calculadora().setVisible(true);
             }
         });
     }
